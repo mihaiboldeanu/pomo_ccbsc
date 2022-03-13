2d20b58e71147a08165dc21e1a545958ed50af64
Robert Gebauer <robert.gebauer@gmx.de>
2022-03-13 10:40:42 +0100

    Bump 0.0.11

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='pomo_ccbsc',
     packages=find_packages(),
-    version='0.0.10',
+    version='0.0.11',
     description='',
     author='',
     url="",
